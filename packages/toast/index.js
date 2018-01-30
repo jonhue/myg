52aa0da761bc4c4627027797de2456ce1349d210
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-01-30 20:46:46 +0100

    add toast component

--- packages/toast/index.js
+++ packages/toast/index.js
@@ -2,6 +2,22 @@ import Myg from '@myg/base';
 import {MDCSnackbar, MDCSnackbarFoundation} from '@material/snackbar';
 
 class Toast extends Myg {
+
+    constructor(el) {
+        this._snackbar = new MDCSnackbar(el);
+    }
+
+    get snackbar() {
+        return this._snackbar;
+    }
+    set snackbar(val) {
+        this._snackbar = val;
+    }
+
+    show(message) {
+        self.snackbar.show({ message: message });
+    }
+
 }
 
 export default Toast;
