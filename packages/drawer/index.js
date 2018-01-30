18d4a09b109036da05eb2992162c74f04e81a276
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-01-30 20:53:19 +0100

    add drawer component

--- packages/drawer/index.js
+++ packages/drawer/index.js
@@ -2,6 +2,28 @@ import Myg from '@myg/base';
 import {MDCTemporaryDrawer, MDCTemporaryDrawerFoundation, util} from '@material/drawer';
 
 class Drawer extends Myg {
+
+    constructor(el) {
+        this._drawer = new MDCTemporaryDrawer(el);
+    }
+
+    get drawer() {
+        return this._drawer;
+    }
+    set drawer(val) {
+        this._drawer = val;
+    }
+
+    open() {
+        this.drawer.open = true;
+    }
+    close() {
+        this.drawer.open = false;
+    }
+    toggle() {
+        this.drawer.open = !this.drawer.open;
+    }
+
 }
 
 export default Drawer;
