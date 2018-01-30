a7f4d935b028d4abdb3308ea237e311f74c4c9d0
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-01-30 21:06:06 +0100

    add checkbox & radio components

--- packages/checkbox/index.js
+++ packages/checkbox/index.js
@@ -2,6 +2,26 @@ import Myg from '@myg/base';
 import {MDCCheckbox, MDCCheckboxFoundation} from '@material/checkbox';
 
 class Checkbox extends Myg {
+
+    constructor(el) {
+        this._checkbox = new MDCCheckbox(el);
+    }
+
+    get checkbox() {
+        return this._checkbox;
+    }
+    set checkbox(val) {
+        this._checkbox = val;
+    }
+
+    static init(el) {
+        let arr = [];
+        for ( let obj of el ) {
+            arr.push(new Checkbox(obj));
+        }
+        return arr;
+    }
+
 }
 
 export default Checkbox;
--- packages/radio/index.js
+++ packages/radio/index.js
@@ -2,6 +2,26 @@ import Myg from '@myg/base';
 import {MDCRadio, MDCRadioFoundation} from '@material/radio';
 
 class Radio extends Myg {
+
+    constructor(el) {
+        this._radio = new MDCRadio(el);
+    }
+
+    get radio() {
+        return this._radio;
+    }
+    set radio(val) {
+        this._radio = val;
+    }
+
+    static init(el) {
+        let arr = [];
+        for ( let obj of el ) {
+            arr.push(new Radio(obj));
+        }
+        return arr;
+    }
+
 }
 
 export default Radio;
