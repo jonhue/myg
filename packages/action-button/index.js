d298aac5476383fb04a3e2fed8a1a6f53a818b94
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-02-02 08:29:13 +0100

    add action button component

--- packages/action-button/index.js
+++ packages/action-button/index.js
@@ -1,6 +1,28 @@
 import Myg from '@myg/base';
 
 class ActionButton extends Myg {
+
+    constructor(el) {
+        this._actionButton = el;
+    }
+
+    get actionButton() {
+        return this._actionButton
+    }
+    set actionButton(val) {
+        this._actionButton = val;
+    }
+
+    showActions() {
+
+    }
+    hideActions() {
+
+    }
+    toggleActions() {
+
+    }
+
 }
 
 export default ActionButton;
