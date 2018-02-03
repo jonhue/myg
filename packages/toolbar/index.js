b18d74546dff8de77d009feb4940ecb7708981ef
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-02-03 09:30:30 +0100

    restructure toolbar component

--- packages/toolbar/index.js
+++ packages/toolbar/index.js
@@ -1,6 +1,34 @@
 import Myg from '@myg/base';
+import {MDCToolbar, MDCToolbarFoundation} from '@material/toolbar';
 
 class Toolbar extends Myg {
+
+    constructor( element, options = {} ) {
+        super;
+        this._mdcToolbar = new MDCToolbar(element);
+    }
+
+    show() {
+        this.element.style.transform = '';
+    }
+    hide() {
+        this.element.style.transform = 'translateY(' + -this.element.offsetHeight + 'px)';
+    }
+
+    onScroll( lastScrollTop, delta ) {
+        let scrollTop = document.querySelector('body').scrollTop;
+
+        if ( Math.abs( lastScrollTop - scrollTop ) <= delta )
+            return;
+
+        if ( scrollTop > lastScrollTop && scrollTop > this.element.offsetHeight )
+            this.hide()
+        else if ( scrollTop + window.height < document.height )
+            this.show();
+
+        return scrollTop;
+    }
+
 }
 
 export default Toolbar;
