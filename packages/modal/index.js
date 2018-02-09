c8309fccad15a4cc0745aa052809120cd8b36f7f
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-02-09 09:30:50 +0100

    update modalist

--- packages/modal/index.js
+++ packages/modal/index.js
@@ -1,7 +1,21 @@
 import Myg from 'myg-base';
-import Modalist from 'modalist/modalist';
+import Modalist from 'modalist';
 
 class Modal extends Myg {
+
+    constructor( element, options = {} ) {
+        super;
+        options.element = element;
+        this._modalist = new Modalist(options);
+    }
+
+    get modalist() {
+        return this._modalist;
+    }
+    set modalist(val) {
+        this._modalist = val;
+    }
+
 }
 
 export default Modal;
