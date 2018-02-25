d1651f925f3e5d35f2e00e079209384866c78c37
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-02-25 12:19:32 +0100

    Minor improvements & bugfixes

--- packages/form-field/index.js
+++ packages/form-field/index.js
@@ -1,20 +1,6 @@
 import Myg from 'myg-base';
-import {MDCFormField, MDCFormFieldFoundation} from '@material/form-field';
 
 class Field extends Myg {
-
-    constructor( element, options = {} ) {
-        super( element, options );
-        this._mdcFormField = new MDCFormField(element);
-    }
-
-    get mdcFormField() {
-        return this._mdcFormField;
-    }
-    set mdcFormField(val) {
-        this._mdcFormField = val;
-    }
-
 }
 
 export default Field;
